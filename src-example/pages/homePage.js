4f71c1de5a4cc2c46e81b813be0c9a7f5cd9a4c8
binkpitch <pitchayutn@gmail.com>
2017-06-04 18:05:33 +0700

    change the look of home page

--- src-example/pages/homePage.js
+++ src-example/pages/homePage.js
@@ -1,18 +1,38 @@
 import React from 'react'
+import { Header, Icon, Message, Label } from 'semantic-ui-react'
 import injectSheet from 'react-jss'
 
 const styles = {
-  display: 'flex',
-  justifyContent: 'center',
-  alignItems: 'center'
+  container: {
+    margin: 64
+  }
 }
 
-const IntroPage = () => {
+const HomePage = () => {
   return (
-    <div style={styles}>
-      <h2>Reactor</h2>
+    <div style={styles.container}>
+      <Header as='h2' icon textAlign='center'>
+        <Icon name='cubes' style={{marginBottom: 16}} />
+        A React boilerplate with Redux
+        <Header.Subheader>
+          <a href='https://github.com/binkpitch/reactor'>https://github.com/binkpitch/reactor</a>
+        </Header.Subheader>
+      </Header>
+      <Message positive style={{marginTop: 56}}>
+        <Message.Header>To install</Message.Header>
+        <p>Run "<b>git clone https://github.com/binkpitch/reactor.git your-project-name && cd your-project-name && rm -rf .git && yarn install</b>"</p>
+      </Message>
+      <Message info>
+        <Message.Header>To see redux in action</Message.Header>
+        <p>Install <a href='https://chrome.google.com/webstore/detail/redux-devtools/lmhkpmbekcpmknklioeibfkpmmfibljd?hl=en'>Redux DevTools</a> (Chrome Extension)</p>
+      </Message>
+      <Label attached='bottom'>
+        <div style={{textAlign: 'center', marginTop: 8, marginBottom: 8}}>
+          This project was bootstrapped with <a href='https://github.com/facebookincubator/create-react-app'>Create React App</a>
+        </div>
+      </Label>
     </div>
   )
 }
 
-export default injectSheet(styles)(IntroPage)
+export default injectSheet(styles)(HomePage)
